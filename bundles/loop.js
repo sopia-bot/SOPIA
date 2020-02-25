4554b87fd851e4bda08239bc99f00069f14afb5f
Tree-Some <youn@tree-some.dev>
2020-02-25 09:48:48 +0900

    반복 설정 매니저만 가능하도록 함.

--- bundles/loop.js
+++ bundles/loop.js
@@ -2,24 +2,26 @@ sopia.var.intvCnt = 0;
 
 sopia.on('message', (e) => {
 	if ( e.isCmd || isCmd(e) ) {
-		if ( e.message.match(/반복\ (삭제|제거)/g) ) {
-			if ( sopia.var.intvCnt > 0 ) {
-				sopia.itv.clear('intv' + (sopia.var.intvCnt - 1));
-				sopia.send("가장 마지막 반복 작업을 제거했습니다.");
-			} else {
-				sopia.send("제거할 반복 작업이 없습니다.");
-			}
-		} else if ( e.message.match(/^반복/) ) {
-			let msg = e.message.replace("반복 ", "");
-			let time = parseInt(msg.match(/^[0-9]+/g)[0], 10);
-			msg = msg.replace(time.toString()+" ", "");
-	
+		if ( isAdmin(e.author) ) {
+			if ( e.message.match(/반복\ (삭제|제거)/g) ) {
+				if ( sopia.var.intvCnt > 0 ) {
+					sopia.itv.clear('intv' + (sopia.var.intvCnt - 1));
+					sopia.send("가장 마지막 반복 작업을 제거했습니다.");
+				} else {
+					sopia.send("제거할 반복 작업이 없습니다.");
+				}
+			} else if ( e.message.match(/^반복/) ) {
+				let msg = e.message.replace("반복 ", "");
+				let time = parseInt(msg.match(/^[0-9]+/g)[0], 10);
+				msg = msg.replace(time.toString()+" ", "");
 
-			const key = 'intv' + (sopia.var.intvCnt++);
-			sopia.itv.add(key, () => {
-				sopia.send(msg);
-			}, (time * 1000));
-			sopia.send(`앞으로 ${(time)}초 마다 반복 채팅을 입력합니다.`);
-		}	
+
+				const key = 'intv' + (sopia.var.intvCnt++);
+				sopia.itv.add(key, () => {
+					sopia.send(msg);
+				}, (time * 1000));
+				sopia.send(`앞으로 ${(time)}초 마다 반복 채팅을 입력합니다.`);
+			}	
+		}
 	}
 });
