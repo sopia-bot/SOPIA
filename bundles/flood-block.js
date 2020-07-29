a41fa3685ea57c43f45452e6440b558aafff9887
Tree Some <youn@tree-some.dev>
2020-07-29 18:30:04 +0900

    [Hot Fix] flood-block.js

--- bundles/flood-block.js
+++ bundles/flood-block.js
@@ -1,6 +1,20 @@
 sopia.var.stack = [];
 sopia.var.kickedStack = [];
 
+updateProps();
+sopia.api.blockUser = (userId) => {
+	sopia.modules.axios({
+		url: `https://kr-api.spooncast.net/lives/${sopia.live.id}/block/`,
+		headers: {
+			'authorization': sopia.var.props.authKey,
+		},
+        method: 'post',
+		data: {
+			block_user_id: Number(userId),
+		},
+	});
+}
+
 checkFlood = (stack) => {
     let lastMessage = stack[0].message;
     let lastId = stack[0].id;
@@ -23,7 +37,34 @@ sopia.on('message', (e) => {
         message: e.message,
         nickname: e.author.nickname
     });
-    
+
+    if ( sopia.var.stack.length >= 10 ) {
+        if ( isAdmin(sopia.me) ) {
+            let copy = JSON.parse(JSON.stringify(sopia.var.stack));
+            if ( sopia.var.kickedStack.includes(copy[0].id) ) {
+                // do nothing
+            } else {
+                if ( checkFlood(copy) === true ) {
+                    sopia.var.kickedStack.push(copy[0].id);
+                    sopia.api.blockUser(copy[0].id);
+                    sopia.send(`${copy[0].nickname} 님을 강퇴합니다.`);
+                }
+            }
+        }
+    }
+})
+
+sopia.on('like', (e) => {
+    if ( sopia.var.stack.length === 10 ) {
+        sopia.var.stack.shift();
+    }
+
+    sopia.var.stack.push({
+        id: e.author.id,
+        message: 'like',
+        nickname: e.author.nickname
+    });
+
     if ( sopia.var.stack.length >= 10 ) {
         if ( isAdmin(sopia.me) ) {
             let copy = JSON.parse(JSON.stringify(sopia.var.stack));
