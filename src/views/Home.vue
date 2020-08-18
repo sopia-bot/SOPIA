36babae8451b5521ba855deab6e2d2ee2a00325f
Tree Some <youn@tree-some.dev>
2020-08-18 18:02:10 +0900

    live card ui definition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,47 +5,66 @@
  * Copyright (c) TreeSome. Licensed under the MIT License.
 -->
 <template>
-	<v-main>
+	<v-main class="indigo lighten-5" style="min-height: 100vh">
 		<v-row v-if="liveList.data" class="ma-0">
 			<v-col
 				v-for="(live, idx) in liveList.data"
 				:key="live.id"
 				cols="12"
 				sm="6"
-				md="4">
+				md="4"
+				lg="3"
+				xl="2">
 				<!-- S:Live Item -->
-				<v-card
-					max-width="400"
-					>
+				<v-hover>
+					<template v-slot="{ hover }">
+						<v-card
+							max-width="300"
+							flat
+							tile
+							color="grey lighten-4"
+							class="mx-auto"
+							style="cursor: pointer"
+							:elevation="hover ? 24 : 6"
+							>
+							<v-img
+								max-width="300"
+								max-height="250"
+								aspect-ratio="1.4"
+								class="elevation-0"
+								:src="live.imgUrl"
+								></v-img>
 
-					
+							<v-card-text>
+								{{ live.title }}
+							</v-card-text>
 
-					<v-card-actions>
-						<v-list-item class="grow">
-							<v-list-item-avatar color="grey darken-3">
-								<v-img
-									class="elevation-6"
-									src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-									></v-img>
-							</v-list-item-avatar>
+							<v-card-actions>
+								<v-list-item class="grow">
+									<v-list-item-avatar>
+										<v-img
+											:src="live.author.profileUrl"></v-img>
+									</v-list-item-avatar>
 
-							<v-list-item-content>
-								<v-list-item-title>Evan You</v-list-item-title>
-							</v-list-item-content>
+									<v-list-item-content>
+										<v-list-item-title>{{ live.author.nickname }}</v-list-item-title>
+									</v-list-item-content>
 
-							<v-row
-								align="center"
-								justify="end"
-								>
-								<v-icon class="mr-1">mdi-heart</v-icon>
-								<span class="subheading mr-2">256</span>
-								<span class="mr-1">·</span>
-								<v-icon class="mr-1">mdi-share-variant</v-icon>
-								<span class="subheading">45</span>
-							</v-row>
-						</v-list-item>
-					</v-card-actions>
-				</v-card>
+									<v-row
+										align="center"
+										justify="end"
+										>
+										<span class="mr-1">·</span>
+										<v-icon class="mr-1">mdi-account</v-icon>
+										<span class="subheading mr-2">{{ live.memberCount }}</span>
+										<v-icon class="mr-1">mdi-heart</v-icon>
+										<span class="subheading mr-2">{{ live.likeCount }}</span>
+									</v-row>
+								</v-list-item>
+							</v-card-actions>
+						</v-card>
+					</template>
+				</v-hover>
 				<!-- E:Live Item -->
 			</v-col>
 		</v-row>
