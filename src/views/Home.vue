3330443454f9fcd31e3210f10f6702c63b8d2be9
Tree Some <youn@tree-some.dev>
2020-08-18 20:02:16 +0900

    apply infinite loading

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,11 @@
  * Copyright (c) TreeSome. Licensed under the MIT License.
 -->
 <template>
-	<v-main class="indigo lighten-5" style="min-height: 100vh">
-		<v-row v-if="liveList.data" class="ma-0">
+	<v-main class="indigo lighten-5" style="max-height: 100vh">
+		<v-row v-if="liveList" class="ma-0" align="center">
 			<v-col
-				v-for="(live, idx) in liveList.data"
-				:key="live.id"
+				v-for="(live, idx) in liveList"
+				:key="'' + idx + live.id"
 				cols="12"
 				sm="6"
 				md="4"
@@ -67,26 +67,68 @@
 				</v-hover>
 				<!-- E:Live Item -->
 			</v-col>
+			<v-col cols="12" align="center">
+				<infinite-loading @infinite="getNextLiveList">
+					<div slot="no-more" class="text-white">
+						<h3 class="indigo--text text--darken-4" >{{ $t('home.load-fin') }}</h3>
+					</div>
+					<div slot="no-results" class="text-white">
+						<h3 class="indigo--text text--darken-4" >{{ $t('home.load-fin') }}</h3>
+					</div>
+				</infinite-loading>
+			</v-col>
 		</v-row>
 	</v-main>
 </template>
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator';
 import GlobalMixins from '../plugins/mixins';
-import { ApiManager, ApiRequest } from 'sopia-core';
+import { ApiManager, ApiRequest, Play } from 'sopia-core';
+import InfiniteLoading from 'vue-infinite-loading';
+import { StateChanger } from 'vue-infinite-loading';
 
-@Component
+const sleep = (msec: number) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, msec);
+	});
+};
+
+
+@Component({
+	components: {
+		InfiniteLoading,
+	},
+})
 export default class Home extends Mixins(GlobalMixins) {
-	public liveList: ApiManager = new ApiManager(new ApiRequest(''));
+	public liveManager!: ApiManager;
+	public liveList: Play[] = [];
+	public asyncMutex: boolean = false;
 
 	// TODO: Can setting audult content
-	public async getNextLiveList() {
-		if ( this.liveList && this.liveList.data ) {
-			this.liveList = await this.liveList.next();
-		} else {
-			this.liveList = await this.$sopia.liveManager.livePopular();
-			console.log(this.liveList);
+	public async getNextLiveList(state?: StateChanger) {
+
+		while ( this.asyncMutex ) {
+			await sleep(100);
 		}
+		this.asyncMutex = true;
+
+		if ( this.liveManager ) {
+			if ( this.liveManager.response.next ) {
+				this.liveManager = await this.liveManager.next();
+				this.liveList = this.liveList.concat(this.liveManager.data);
+			}
+
+			if ( this.liveManager.response.next === '' ) {
+				state?.complete();
+			} else {
+				state?.loaded();
+			}
+		} else {
+			this.liveManager = await this.$sopia.liveManager.livePopular();
+			this.liveList = this.liveManager.data;
+		}
+
+		this.asyncMutex = false;
 	}
 
 	public async mounted() {
