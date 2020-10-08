612ecf5b3c47983915b78694a1a011a7f94a83be
Tree Some <youn@tree-some.dev>
2020-10-08 11:41:41 +0900

    search complete

--- src/views/Search/Index.vue
+++ src/views/Search/Index.vue
@@ -5,31 +5,75 @@
  * Copyright (c) TreeSome. Licensed under the MIT License.
 -->
 <template>
-	<v-main class="custom indigo lighten-5">
+	<v-main class="custom indigo lighten-5" style="min-height: 100vh;">
 		<search-header></search-header>
-		<v-btn-toggle
-			v-model="searchType"
-			tile
-			color="indigo accent-3"
-			group
-			>
+		<v-container>
+			<v-btn-toggle
+				v-model="searchType"
+				tile
+				color="indigo accent-3"
+				group
+				>
 
-			<v-btn
-				v-for="search of searchTypes"
-				:key="search.value"
-				:value="search.value"
-				@click="searchContent(search.value)">
-				{{ search.text }}
-			</v-btn>
-		</v-btn-toggle>
-		<infinite-loading @infinite="getNextSearchList">
-			<div slot="no-more" class="text-white">
-				<h3 class="indigo--text text--darken-4" >{{ $t('home.load-fin') }}</h3>
-			</div>
-			<div slot="no-results" class="text-white">
-				<h3 class="indigo--text text--darken-4" >{{ $t('home.load-fin') }}</h3>
-			</div>
-		</infinite-loading>
+				<v-btn
+					v-for="search of searchTypes"
+					:key="search.value"
+					:value="search.value"
+					@click="searchContent(search.value)">
+					{{ search.text }}
+				</v-btn>
+			</v-btn-toggle>
+
+			<v-divider class="my-6 mx-2"></v-divider>
+
+			<v-row class="ma-0">
+				<v-col
+					cols="12"
+					sm="6"
+					v-for="(search, idx) of searchList"
+					:key="searchType + idx">
+					<div v-if="searchType === 'user'">
+						<v-list-item>
+								<v-badge
+									v-if="search.currentLive"
+									avatar
+									bottom
+									color="red accent-4"
+									dot
+									offset-x="25"
+									offset-y="17">
+									<v-list-item-avatar class="ml-0">
+										<v-img :src="search.profileUrl"></v-img>
+									</v-list-item-avatar>
+								</v-badge>
+								<v-list-item-avatar v-else>
+									<v-img :src="search.profileUrl"></v-img>
+								</v-list-item-avatar>
+
+							<v-list-item-content>
+								<v-list-item-title>
+									<router-link class="blue-grey--text" :to="`/users/${search.id}/`">{{ search.nickname }}</router-link>
+								</v-list-item-title>
+								<v-list-item-subtitle>@{{ search.tag }}</v-list-item-subtitle>
+							</v-list-item-content>
+						</v-list-item>
+					</div>
+					<div v-else>
+						<live-item :live="search"></live-item>
+					</div>
+				</v-col>
+				<v-col cols="12">
+					<infinite-loading @infinite="getNextSearchList">
+						<div slot="no-more" class="text-white">
+							<h3 class="indigo--text text--darken-4" >{{ $t('home.load-fin') }}</h3>
+						</div>
+						<div slot="no-results" class="text-white">
+							<h3 class="indigo--text text--darken-4" >{{ $t('home.load-fin') }}</h3>
+						</div>
+					</infinite-loading>
+				</v-col>
+			</v-row>
+		</v-container>
 	</v-main>
 </template>
 <script lang="ts">
@@ -39,6 +83,7 @@ import { ApiManager, Play, User, ContentType } from 'sopia-core';
 import InfiniteLoading from 'vue-infinite-loading';
 import { StateChanger } from 'vue-infinite-loading';
 import SearchHeader from './Header.vue';
+import LiveItem from '../Home/LiveItem.vue';
 
 const sleep = (msec: number) => {
 	return new Promise((resolve, reject) => {
@@ -50,6 +95,7 @@ const sleep = (msec: number) => {
 	components: {
 		InfiniteLoading,
 		SearchHeader,
+		LiveItem,
 	},
 })
 export default class Search extends Mixins(GlobalMixins) {
@@ -115,3 +161,5 @@ export default class Search extends Mixins(GlobalMixins) {
 	}
 }
 </script>
+<style scope>
+</style>
