b40f4efff01f321183406b8b90af82a6718a6113
FioraPellerin <ifthe1201@naver.com>
2021-05-04 15:41:33 +0900

    neuro rollback

--- src/resources/js/sopia.js
+++ src/resources/js/sopia.js
@@ -5,7 +5,6 @@
 ///////////////////////////////////////////////////////////////
 
 const { LiveEvent } = require('sopia-core');
-const Neuro = require('./neuro.js');
 
 /**
  * @sopia 의 객체를 생성한다.
@@ -64,7 +63,6 @@ sopia.api = {
 };
 
 sopia.tts = speech;
-sopia.neuro = new Neuro();
 
 sopia.var = {};
 sopia.storage = {
