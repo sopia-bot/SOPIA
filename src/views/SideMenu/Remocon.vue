ed4133b930ce42022ca2a671091a9e61583b8ea8
raravel <youn@tree-some.dev>
2022-02-15 17:08:22 +0900

    메뉴 버튼 위치 설정 가능 및 저장

--- src/views/SideMenu/Remocon.vue
+++ src/views/SideMenu/Remocon.vue
@@ -3,8 +3,12 @@
 		tile dark fixed
 		color="indigo darken-4"
 		class="side-menu-btn"
-		:style="{ left, }"
-		@mouseover="hover">
+		ref="menu"
+		:style="{ left, top, }"
+		@mouseover="hover"
+		@mousedown="mousedown"
+		@mousemove="mousemove"
+		@mouseup="mouseup">
 		Menu
 	</v-btn>
 </template>
@@ -17,6 +21,11 @@ export default class SideMenuRemocon extends Mixins(GlobalMixins) {
 	/* close: -32px, open 256px - 32px */
 	public closedLeft: number = -32;
 	public sideWidth: number = 256;
+	public defaultTop: string = 'calc(100vh / 2 - (32px))';
+	public setTop: number = -1;
+
+	private trigger = false;
+	private beforeEvt!: MouseEvent;
 
 	get left() {
 		return this.$store.state.sideopen ?
@@ -24,9 +33,46 @@ export default class SideMenuRemocon extends Mixins(GlobalMixins) {
 			`${this.closedLeft}px`;
 	}
 
+	get top() {
+		if ( this.setTop >= 0 ) {
+			return `${this.setTop}px`;
+		}
+		return this.defaultTop;
+	}
+
+	public mounted() {
+		this.setTop = this.$cfg.get('menu-top') ?? -1;
+	}
+
 	public hover() {
-		console.log('call hover');
 		this.$store.state.sideopen = true;
+	}
+
+	public mousedown() {
+		this.trigger = true;
+	}
+
+	public mousemove(evt: MouseEvent) {
+		if ( this.trigger ) {
+			if ( this.beforeEvt ) {
+				const rect = (this.$refs.menu as Vue)?.$el.getBoundingClientRect();
+				if ( rect.y <= 0 && (evt.y <= this.beforeEvt.y) ) {
+					return;
+				}
+
+				if ( (rect.y + rect.height) >= window.innerHeight && (evt.y >= this.beforeEvt.y) ) {
+					return;
+				}
+			}
+			this.setTop = evt.y;
+			this.beforeEvt = evt;
+		}
+	}
+
+	public mouseup() {
+		this.$cfg.set('menu-top', this.setTop);
+		this.$cfg.save();
+		this.trigger = false;
 	}
 }
 </script>
@@ -35,7 +81,7 @@ export default class SideMenuRemocon extends Mixins(GlobalMixins) {
 	transform: rotate(90deg);
 	z-index: 10;
 	width: 100px;
-	top: calc(100vh / 2 - (32px));
+	
 	transition-property: left;
 }
 </style>
