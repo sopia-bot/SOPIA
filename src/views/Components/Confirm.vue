273c90d5c4a20c5ebe5112d30129079986fc5dba
raravel <youn@tree-some.dev>
2022-03-21 16:48:17 +0900

    Modal, Confirm 디자인 변경

--- src/views/Components/Confirm.vue
+++ src/views/Components/Confirm.vue
@@ -10,17 +10,17 @@
 		persistent
 		max-width="500px"
 		width="70%">
-		<v-card align="center">
+		<v-card color="blue-grey darken-4" dark tile>
 			<v-card-text class="pt-8 py-6">
 				<v-row class="ma-0" align="center">
 					<v-col cols="12" align="center">
-						<h1 class="mb-8 d-flex">
-							<span class="mx-auto d-flex" style="align-items: center">
+						<h1 class="mb-8 d-flex text-h3 font-weight-bold">
+							<span class="mx-auto d-flex indigo--text text--accent-1" style="align-items: center">
 								<v-icon :style="{ color: colors[type] }" class="mr-2" large>{{ icons[type] }}</v-icon>
 								{{ title }}
 							</span>
 						</h1>
-						<span class="subtitle-1" v-html="content"></span>
+						<span class="text-subtitle-1 blue-grey--text text--lighten-4" v-html="content"></span>
 					</v-col>
 				</v-row>
 				<v-row class="ma-0 mt-4" align="center">
@@ -28,17 +28,20 @@
 						<div class="flex-grow-1"></div>
 
 						<v-btn
-							color="red darken-1"
-	   						class="mr-4"
-							large
-							dark
+							color="blue-grey darken-2"
+							class="mr-4 red--text text--lighten-4"
+							large depressed
+							rounded
+							width="120px"
 							@click="$emit('cancel', $event); $emit('update:open', false);">
 							{{ textCancel }}
 						</v-btn>
 						<v-btn
-							color="indigo"
-							large
-							dark
+							color="white"
+							class="indigo--text"
+							large depressed
+							rounded
+							width="120px"
 							@click="$emit('ok', $event); $emit('update:open', false);">
 							{{ textOk }}
 						</v-btn>
--- src/views/Components/Modal.vue
+++ src/views/Components/Modal.vue
@@ -10,17 +10,17 @@
 		persistent
 		max-width="500px"
 		width="70%">
-		<v-card align="center">
+		<v-card color="blue-grey darken-4" dark tile>
 			<v-card-text class="pt-8 py-6">
 				<v-row class="ma-0" align="center">
 					<v-col cols="12" align="center">
-						<h1 class="mb-8 d-flex">
-							<span class="mx-auto d-flex" style="align-items: center">
+						<h1 class="mb-8 d-flex text-h3 font-weight-bold">
+							<span class="mx-auto d-flex indigo--text text--accent-1" style="align-items: center">
 								<v-icon :style="{ color: colors[type] }" class="mr-2" large>{{ icons[type] }}</v-icon>
 								{{ title }}
 							</span>
 						</h1>
-						<span class="subtitle-1" v-html="content"></span>
+						<span class="text-subtitle-1 blue-grey--text text--lighten-4" v-html="content"></span>
 					</v-col>
 				</v-row>
 				<v-row class="ma-0 mt-4" align="center">
@@ -28,9 +28,11 @@
 						<div class="flex-grow-1"></div>
 
 						<v-btn
-							color="indigo"
-							large
-							dark
+							color="white"
+							class="indigo--text"
+							large depressed
+							rounded
+							width="120px"
 							@click="$emit('ok', $event); $emit('update:open', false);">
 							{{ textOk }}
 						</v-btn>
