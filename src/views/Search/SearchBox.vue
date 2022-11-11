ed6250513201ad06e5bd250c3c5886a76bf2727f
raravel <youn@tree-some.dev>
2022-11-11 11:38:01 +0900

    #38 Preview Search user list

--- src/views/Search/SearchBox.vue
+++ src/views/Search/SearchBox.vue
@@ -1,5 +1,5 @@
 <template>
-	<div style="display: contents; position: relative;">
+	<div style="display: contents; position: relative;" @click.stop="() => {}">
 		<v-text-field
 			ref="searchBox"
 			v-model="searchText"
@@ -16,7 +16,35 @@
 			:elevation="8"
 			style="position: fixed;"
 			:style="boxStyle">
-			<p v-for="user in users" :key="user.tag">{{ user.nickname }}</p>
+			<v-list>
+				<v-list-item v-for="user in users" :key="user.tag">
+					<v-badge
+						v-if="user.current_live_id"
+						avatar
+						bottom
+						color="red accent-4"
+						dot
+						offset-x="25"
+						offset-y="17">
+						<v-list-item-avatar
+							@click="$evt.$emit('live-join', user.current_live_id)"
+							style="cursor: pointer;"
+							class="ml-0 grey lighten-2">
+							<v-img :src="user.profile_url"></v-img>
+						</v-list-item-avatar>
+					</v-badge>
+					<v-list-item-avatar v-else class="grey lighten-2">
+						<v-img :src="user.profile_url"></v-img>
+					</v-list-item-avatar>
+
+					<v-list-item-content>
+						<v-list-item-title>
+							<router-link class="blue-grey--text" :to="`/user/${user.id}/`" @click="outerClick">{{ user.nickname }}</router-link>
+						</v-list-item-title>
+						<v-list-item-subtitle>@{{ user.tag }}</v-list-item-subtitle>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
 		</v-card>
 	</div>
 </template>
@@ -32,9 +60,22 @@ export default class PreviewList extends Mixins(GlobalMixins) {
 	public boxStyle = {
 		left: '0',
 		top: '0',
+		width: '0',
+		display: 'none',
 	};
 	public increment = 0;
 	public users: User[] = [];
+
+	public mounted() {
+		this.outerClick = this.outerClick.bind(this);
+		this.$nextTick(() => {
+			window.addEventListener('click', this.outerClick);
+		});
+	}
+
+	public beforeUnmount() {
+		window.removeEventListener('click', this.outerClick);
+	}
 
 	public async searchContent() {
 		const text = encodeURI(this.searchText);
@@ -49,6 +90,7 @@ export default class PreviewList extends Mixins(GlobalMixins) {
 		}
 		this.boxStyle.left = Math.floor(this.getCardLeft()) + 'px';
 		this.boxStyle.top = Math.floor(this.getCardTop()) + 'px';
+		this.boxStyle.width = Math.floor(this.getBoxRect().width) + 'px';
 
 		if ( this.searchText.trim().length > 0 ) {
 			this.increment += 1;
@@ -68,12 +110,12 @@ export default class PreviewList extends Mixins(GlobalMixins) {
 				keyword: this.searchText,
 			},
 		});
+		this.boxStyle.display = 'block';
 		this.users = req.res.results.splice(0, 5);
 	}
 
 	public getBoxRect(): DOMRect {
 		const box = (this.$refs.searchBox as Vue)?.$el as HTMLElement;
-		console.log('refs', this.$refs);
 		return box?.getBoundingClientRect();
 	}
 
@@ -83,6 +125,10 @@ export default class PreviewList extends Mixins(GlobalMixins) {
 
 	public getCardTop() {
 		return (this.getBoxRect()?.top + this.getBoxRect()?.height) + 10 || 0;
+	}
+
+	public outerClick() {
+		this.boxStyle.display = 'none';
 	}
 
 }
