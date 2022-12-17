5bca3146939b79370ca1c6645e31e5e29bada8b1
tree-some <youn@tree-some.dev>
2022-12-18 01:35:18 +0900

    :see_no_evil: build directory remove in .gitignore (#43)

--- build/gulpfile.app.js
+++ build/gulpfile.app.js
@@ -11,14 +11,14 @@ gulp.task('app:typescript', function() {
   return project.src()
     .pipe(project())
     .js
-    .pipe(gulp.dest('packages/app/build'));
+    .pipe(gulp.dest('packages/app/.build'));
 });
 
 gulp.task('app:icon-build', () => npm('run', 'icon-build', '--prefix', 'packages/app'));
 gulp.task('app:copy', () => gulp.src([
   'packages/app/package.json',
   'packages/app/electron.build.json',
-]).pipe(gulp.dest('packages/app/build')));
+]).pipe(gulp.dest('packages/app/.build')));
 gulp.task('app:electron', function (done) {
   builder.build({
     projectDir: 'packages/app',
