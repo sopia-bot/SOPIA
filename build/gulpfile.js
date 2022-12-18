ffaa663e15ba7e9a5f16c21b642b6bc13ec59c95
tree-some <youn@tree-some.dev>
2022-12-18 10:08:56 +0900

    :construction: Package dependecy needs core (#46)

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -4,4 +4,4 @@ require('./gulpfile.app');
 require('./gulpfile.core');
 require('./gulpfile.view');
 
-gulp.task('default', gulp.parallel(['core', 'app', 'view']));
+gulp.task('default', gulp.series(['core', 'app', 'view']));
