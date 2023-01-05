8fc2fa1dc3d75ebc38f4c2e6ee49fc1f2f2037cd
raravel <youn@tree-some.dev>
2023-01-05 15:43:59 +0900

    :fire: remove undefined import

--- packages/app/src/app.module.ts
+++ packages/app/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { GlobalModule } from './global.module';
-import { TestModule } from './test/test.module';
 import { AppModule as ApplicationModule } from './app/app.module';
 import { SpoonModule } from './spoon/spoon.module';
 
