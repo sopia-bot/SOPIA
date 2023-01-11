e3f677b837d73af223fd372810819123b0675afe
raravel <youn@tree-some.dev>
2023-01-11 11:34:51 +0900

    :green_heart: Error fix cannot overwrite d.ts file

--- packages/context-bridge/rollup.config.js
+++ packages/context-bridge/rollup.config.js
@@ -1,6 +1,9 @@
 const typescript = require('rollup-plugin-typescript2');
 const commonjs = require('@rollup/plugin-commonjs');
 const resolve = require('@rollup/plugin-node-resolve');
+const fs = require('fs');
+
+
 
 module.exports = [
 	{
@@ -31,6 +34,14 @@ module.exports = [
 		},
 		external: ['electron', 'crypto-js'],
 		plugins: [
+			{
+				name: 'rm',
+				buildStart() {
+					fs.rmSync('./types', {
+						recursive: true,
+					});
+				},
+			},
 			typescript({
         exclude: 'node_modules/**',
 				tsconfig: 'tsconfig.renderer.json',
