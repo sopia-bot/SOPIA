33656ecc347932c025610597805daa39e919a309
raravel <youn@tree-some.dev>
2023-01-19 14:33:02 +0900

    :recycle: Division fix manager input component from live-setting-content (#49)

--- packages/view-novalt/src/components/bottom-bar/live-setting-content.tsx
+++ packages/view-novalt/src/components/bottom-bar/live-setting-content.tsx
@@ -1,22 +1,15 @@
-import { LiveCategoryList, User } from "@sopia-bot/core";
+import { LiveCategoryList } from "@sopia-bot/core";
 import { Chips } from "primereact/chips";
 import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
-import { ListBox } from "primereact/listbox";
 import { MultiSelect } from "primereact/multiselect";
-import { OverlayPanel } from "primereact/overlaypanel";
 import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useSpoon } from "../../plugins/spoon";
-import { ToggleButton, ToggleButtonChangeParams } from 'primereact/togglebutton'
-import { useQuery } from "@tanstack/react-query";
 import { FileUpload } from 'primereact/fileupload';
 import { Button } from "primereact/button";
 import { LiveSettingDto } from '@sopia-bot/bridge/dist/dto';
+import FixManagerInput from "./manager-input";
 
-type Manager = User & {
-	isManager: boolean;
-}
 
 export type LiveSettingContentProps = {
   value: LiveSettingDto,
@@ -28,19 +21,12 @@ export default function LiveSettingContent(props: LiveSettingContentProps) {
   const [notice, setNotice] = useState(props.value.welcome_message);
   const [selectedCategories, setCategories] = useState(props.value.categories);
   const [tags, setTags] = useState<string[]>(props.value.tags);
-  const [fixedManagerSearch, setFixedManagerSearch] = useState('');
-  const [followers, setFollowers] = useState<Manager[]>([]);
+  
   const [image, setImage] = useState<Buffer>(props.value.image || Buffer.from([]));
   const [imgUrl, setImgUrl] = useState('');
-  const searchResultPanel = useRef<OverlayPanel>(null);
   const searchInput = useRef(null);
   const imageUploadRef = useRef(null);
   const { t } = useTranslation();
-  const spoon = useSpoon();
-  const { data: managerList, isSuccess } = useQuery({
-    queryKey: ['getFixedManagers'],
-    queryFn: async () => (await spoon.api.users.manager()).res.results,
-  });
 
   const categoryItemTemplate = (option: (typeof LiveCategoryList)[0]) => (
     <div>
@@ -58,47 +44,6 @@ export default function LiveSettingContent(props: LiveSettingContentProps) {
     </span>
   };
 
-  const [tout, setTout] = useState<NodeJS.Timer|null>(null);
-  const searchFollowers = async () => {
-    if ( !isSuccess ) return;
-    const req = await spoon.api.users.followers(spoon.logonUser, {
-      params: {
-        nickname: fixedManagerSearch,
-      },
-    });
-    searchResultPanel.current?.show(null, searchInput.current);
-    setFollowers(
-			req.res.results.map((user) => ({
-				...user,
-				isManager: !!managerList.find(u => u.id === user.id),
-			}) as Manager)
-		);
-  }
-
-  const setManager = async (e: ToggleButtonChangeParams, user: Manager) => {
-    const idx = managerList?.findIndex((u) => u.id === user.id) || -1;
-    if ( user.isManager ) {
-      await spoon.api.users.deleteManager(user.id); 
-      user.isManager = false;
-      if ( idx >= 0 ) {
-        managerList?.splice(idx, 1);
-      }
-    } else {
-      await spoon.api.users.setManager(user.id);
-      user.isManager = true;
-      if ( idx >= 0 ) {
-        managerList?.push(user);
-      }
-    }
-    setFollowers((followers || []).map((u) => {
-      if ( u.id === user.id ) {
-        return user;
-      }
-      return u;
-    }));
-    e.preventDefault();
-  }
-
   const onSelectImage = async (e: any) => {
     const buffer = await e.files[0].arrayBuffer();
     setImage(Buffer.from(buffer));
@@ -108,17 +53,6 @@ export default function LiveSettingContent(props: LiveSettingContentProps) {
     setImage(Buffer.from([]));
     setImgUrl('');
   }
-
-  useEffect(() => {
-    if ( fixedManagerSearch ) {
-      searchResultPanel.current?.hide();
-      if ( tout ) {
-        clearTimeout(tout);
-        setTout(null);
-      }
-      setTout(setTimeout((() => searchFollowers()), 1000));
-    }
-  }, [fixedManagerSearch]);
 
   useEffect(() => {
     props.onChange({
@@ -199,45 +133,13 @@ export default function LiveSettingContent(props: LiveSettingContentProps) {
             style={{ width: '100%' }}
             ref={searchInput}>
             <label htmlFor='fixed-manager' className='block text-sm'>{t('live.fixed_manager')}</label>
-            <InputText
-              value={fixedManagerSearch}
-              onChange={(e) => setFixedManagerSearch(e.target.value)}
-              id='fixed-manager'
-              placeholder={t('live.fixed_manager_placeholder') || ''}
-              style={{ width: '100%' }} />
-            <OverlayPanel ref={searchResultPanel} id="search-panel">
-              <ListBox
-                options={followers}
-                itemTemplate={(user: Manager) =>
-                  <div className='flex align-items-center' onClick={() => {}}>
-                    <div style={{
-                      backgroundImage: `url(${user.profile_url})`,
-                      width: '30px',
-                      height: '30px',
-                      borderRadius: '25px',
-                      backgroundSize: 'contain',
-                    }} className="mr-2"/>
-                    <span className='mr-2'>{user.nickname}</span>
-                    <span className='mr-2'>({user.tag})</span>
-										<ToggleButton
-                      checked={user.isManager}
-                      className='p-button-sm'
-                      style={{
-                        padding: '0.15rem 0.8rem',
-                      }}
-                      onChange={(e) => setManager(e, user)}
-                      onLabel={t('live.release')||''}
-                      offLabel={t('live.assign')||''} />
-                  </div>
-                }/>
-            </OverlayPanel>
+            <FixManagerInput searchInput={searchInput} />
           </span>
         </div>
         <div className='flex'>
           <span
             className='field'
-            style={{ width: '100%' }}
-            ref={searchInput}>
+            style={{ width: '100%' }}>
             <label htmlFor='upload-image' className='block text-sm'>{t('live.upload_image')}</label>
             <FileUpload
               ref={imageUploadRef}
